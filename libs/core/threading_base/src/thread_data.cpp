10ac8d52803c4e40e5f68bde135ae165b1577fca
v4xsh <vanshdobhal11@gmail.com>
2026-05-05 23:19:52 +0530

    threading_base: add ITT thread init data
    Signed-off-by: v4xsh <vanshdobhal11@gmail.com>

--- libs/core/threading_base/src/thread_data.cpp
+++ libs/core/threading_base/src/thread_data.cpp
@@ -556,4 +556,22 @@ namespace hpx::threads {
     }
 #endif
 
+#if defined(HPX_HAVE_ITTNOTIFY) && HPX_HAVE_ITTNOTIFY != 0 &&                 \
+    !defined(HPX_HAVE_APEX)
+    tracing::thread_region_init_data get_thread_region_init_data(
+        thread_data const* thrdptr)
+    {
+        threads::thread_description const desc = thrdptr->get_description();
+        if (desc.kind() ==
+            threads::thread_description::data_type::description)
+        {
+            return {desc.get_description(), thrdptr->get_thread_phase(),
+                thrdptr, thrdptr->is_stackless(), 0, false,
+                desc.get_description_itt().handle_};
+        }
+        return {"address", thrdptr->get_thread_phase(), thrdptr,
+            thrdptr->is_stackless(), desc.get_address(), true, nullptr};
+    }
+#endif
+
 }    // namespace hpx::threads
