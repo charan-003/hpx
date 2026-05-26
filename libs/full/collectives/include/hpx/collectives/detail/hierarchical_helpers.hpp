5c8a0c0ed791ebd02aa4d3c7248e97927aeed4bc
iemAnshuman <asquare567@gmail.com>
2026-05-26 15:21:39 +0530

    Add partition helpers for hierarchical all_to_all collective
    Signed-off-by: iemAnshuman <asquare567@gmail.com>

---
+++ libs/full/collectives/include/hpx/collectives/detail/hierarchical_helpers.hpp
@@ -0,0 +1,74 @@
+//  Copyright (c) 2026 Anshuman Agrawal
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+/// \file detail/hierarchical_helpers.hpp
+
+#pragma once
+
+#include <hpx/config.hpp>
+
+#include <cstddef>
+#include <vector>
+
+namespace hpx::collectives::detail {
+
+    struct top_level_group
+    {
+        std::size_t left;
+        std::size_t right;
+        std::size_t size;
+    };
+
+    // Compute the top-level partition of [0, num_sites) into `arity` groups.
+    // Uses the same division logic as recursively_fill_communicators:
+    // first (num_sites % arity) groups get ceil(num_sites/arity) sites,
+    // the rest get floor(num_sites/arity).
+    inline std::vector<top_level_group> get_top_level_groups(
+        std::size_t num_sites, std::size_t arity)
+    {
+        if (arity > num_sites)
+        {
+            arity = num_sites;
+        }
+
+        std::size_t const division_steps = num_sites / arity;
+        std::size_t const remainder = num_sites % arity;
+
+        std::vector<top_level_group> groups;
+        groups.reserve(arity);
+
+        std::size_t offset = 0;
+        for (std::size_t i = 0; i != arity; ++i)
+        {
+            std::size_t const group_size =
+                division_steps + (i < remainder ? 1 : 0);
+
+            std::size_t const left = offset;
+            std::size_t const right = left + group_size - 1;
+            groups.push_back(top_level_group{left, right, group_size});
+
+            offset += group_size;
+        }
+
+        return groups;
+    }
+
+    // Return the index of the top-level group that contains `this_site`.
+    inline std::size_t classify_site(
+        std::size_t this_site, std::vector<top_level_group> const& groups)
+    {
+        for (std::size_t i = 0; i != groups.size(); ++i)
+        {
+            if (this_site >= groups[i].left && this_site <= groups[i].right)
+            {
+                return i;
+            }
+        }
+
+        return static_cast<std::size_t>(-1);
+    }
+
+}    // namespace hpx::collectives::detail
