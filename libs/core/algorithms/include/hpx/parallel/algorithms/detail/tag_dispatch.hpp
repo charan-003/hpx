5ae479c21a02b989bc63ffc6e5f191d43e3fbff7
Sai Charan <yadavcharan003@gmail.com>
2026-07-16 03:06:18 -0500

    remove tag invoke from parallel algorithms
    Signed-off-by: Sai Charan <yadavcharan003@gmail.com>

---
+++ libs/core/algorithms/include/hpx/parallel/algorithms/detail/tag_dispatch.hpp
@@ -0,0 +1,86 @@
+//  Copyright (c) 2026 Sai Charan Arvapally
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <hpx/config.hpp>
+// TODO(tag_invoke-removal): drop with has_legacy_tag_invoke in upcoming PR.
+#include <hpx/modules/tag_invoke.hpp>
+
+#include <utility>
+
+namespace hpx::detail {
+
+    // Detects whether an ADL customization point `hpx_invoke(tag, args...)`
+    // has been defined for the given CPO `Tag`. This is the replacement hook
+    // for external customizations of core algorithm CPOs (e.g. by
+    // full/segmented_algorithms), used in place of `tag_invoke`.
+    template <typename Tag, typename... Args>
+    concept has_hpx_invoke = requires(Tag const& tag, Args&&... args) {
+        hpx_invoke(tag, HPX_FORWARD(Args, args)...);
+    };
+
+    // TODO(tag_invoke-removal): drop once full/segmented_algorithms migrate to
+    // `hpx_invoke` and upcoming PR removes the legacy probe below.
+    template <typename Tag, typename... Args>
+    concept has_legacy_tag_invoke = requires(Tag const& tag, Args&&... args) {
+        tag_invoke(tag, HPX_FORWARD(Args, args)...);
+    };
+
+    // CRTP mixin providing the dispatch logic for converted algorithm CPOs
+    // that used to derive (possibly indirectly) from
+    // hpx::functional::detail::tag_fallback<Tag>. Dispatch order:
+    //
+    //   1. hpx_invoke(tag, args...)       external customization (new hook)
+    //   2. tag_invoke(tag, args...)       legacy external customization
+    //   3. Tag::invoke_default(args...) algorithm default (was tag_fallback_invoke)
+    //   4. Base::operator()(args...)      sender/policy/scheduler (e.g.
+    //                                     tag_parallel_algorithm)
+    //
+    // `Base` keeps working exactly as before (unmodified); this mixin only
+    // adds an additional, higher-priority overload on top without touching
+    // shared infrastructure, so each CPO can be converted independently of
+    // every other one.
+    template <typename Tag, typename Base>
+    struct tag_dispatch : Base
+    {
+        template <typename... Args>
+            requires(
+                has_hpx_invoke<Tag, Args...> ||
+                has_legacy_tag_invoke<Tag, Args...> ||
+                requires(Args&&... args) {
+                    Tag::invoke_default(HPX_FORWARD(Args, args)...);
+                } ||
+                requires(Base const& base, Args&&... args) {
+                    base(HPX_FORWARD(Args, args)...);
+                })
+        decltype(auto) operator()(Args&&... args) const
+        {
+            Tag const& tag = static_cast<Tag const&>(*this);
+            if constexpr (has_hpx_invoke<Tag, Args...>)
+            {
+                return hpx_invoke(tag, HPX_FORWARD(Args, args)...);
+            }
+            // TODO(tag_invoke-removal): remove this branch in upcoming PR
+            else if constexpr (has_legacy_tag_invoke<Tag, Args...>)
+            {
+                return tag_invoke(tag, HPX_FORWARD(Args, args)...);
+            }
+            else if constexpr (requires {
+                                   Tag::invoke_default(
+                                       HPX_FORWARD(Args, args)...);
+                               })
+            {
+                return Tag::invoke_default(HPX_FORWARD(Args, args)...);
+            }
+            else
+            {
+                return static_cast<Base const&>(*this)(
+                    HPX_FORWARD(Args, args)...);
+            }
+        }
+    };
+}    // namespace hpx::detail
